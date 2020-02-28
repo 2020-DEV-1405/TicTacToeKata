988d9d6d11643870b88d11996624b9dbed3b8e4e
2020-DEV-1405 <dev2020.014@gmail.com>
2020-02-28 23:54:50 +0530

    Committing enum constant class that tracks state of board which can be blank, cross or circle

---
+++ src/main/java/com/tictactoe/kata/game/GameStateEnum.java
@@ -0,0 +1,11 @@
+package com.tictactoe.kata.game;
+
+/**
+ * It is enum which defines state of tic tac toe game at any point
+ * 
+ * @author AKS1405
+ *
+ */
+public enum GameStateEnum {
+	BLANK, CROSS, CIRCLE
+}
