d4e5fcee344a27bedc828851793e79abc0b2b145
2020-DEV-1405 <dev2020.014@gmail.com>
2020-02-29 00:27:37 +0530

    Adding Implementation of winner logic for the scenario when any player has same state like circle or cross in one row in game processor file over the game board

--- src/main/java/com/tictactoe/kata/game/TicTacToeGameProcessor.java
+++ src/main/java/com/tictactoe/kata/game/TicTacToeGameProcessor.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Component;
 public class TicTacToeGameProcessor {
 
 	private GameStateEnum[][] board = new GameStateEnum[3][3];
+	
+	private GameStateEnum winner = GameStateEnum.BLANK;
 
 	// Initialize board array with blank state of game in constructor
 	public TicTacToeGameProcessor() {
@@ -58,24 +60,67 @@ public class TicTacToeGameProcessor {
 	 */
 	private boolean mark(int row, int col, GameStateEnum gameState) {
 		if (board[row][col] == GameStateEnum.BLANK) {
-			board[row][col] = gameState;			
+			board[row][col] = gameState;		
+			
+			// Checks whether winner has been found post every players chance of marking
+			setWinner(row, col, gameState);
 			return true;
 		}
 		return false;
+	}
+	
+	/**
+	 * This method finds winner after each player plays and marks O or X based on
+	 * condition of winner based on Tic Tac Toe game rules
+	 * 
+	 * @param row
+	 * @param col
+	 * @param gameState
+	 */
+	public void setWinner(int row, int col, GameStateEnum gameState) {
+		validateThreeSameStateInRow(row, gameState);		
+	}
+	
+	/**
+	 * This method finds that if one row has same sign and changes winner state to
+	 * that sign like O or X
+	 * 
+	 * @param row
+	 * @param gameState
+	 */
+	private void validateThreeSameStateInRow(int row, GameStateEnum gameState) {
+		for (int col = 0; col < board.length; col++) {
+			if (board[row][col] != gameState) {
+				break;
+			}
+
+			if (col == 2) {
+				winner = gameState;
+			}
+		}
+	}
+
+	/**
+	 * This method returns state of winner of any time
+	 * 
+	 * @return GameStateEnum the winner
+	 */
+	public GameStateEnum getWinner() {
+		return this.winner;
 	}
 
 		
 	/**
 	 * This method gives current state of board at any time
 	 * 
-	 * @return GAME_STATE[][]
+	 * @return GameStateEnum[][]
 	 */
 	public GameStateEnum[][] getBoard() {
 		return this.board;
 	}
 
 	/**
-	 * This method finds whether at any location on board is marked with circle or
+	 * This method finds whether at any location on board is marked with cross or
 	 * not
 	 * 
 	 * @param row
@@ -96,5 +141,16 @@ public class TicTacToeGameProcessor {
 	 */
 	public boolean isCircle(int row, int col) {
 		return board[row][col] == GameStateEnum.CIRCLE;
+	}
+	
+	/**
+	 * This method used to check whether winner has been found after every chance
+	 * played by player. If state of winner is changed from Blank to Cicrle and
+	 * Cross we can say that one of player has won the game.
+	 * 
+	 * @return boolean
+	 */
+	public boolean isFinished() {
+		return this.winner != GameStateEnum.BLANK;
 	}
 }
