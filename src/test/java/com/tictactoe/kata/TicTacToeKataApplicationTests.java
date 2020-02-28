1a98cfbdcbbc2144d767b83b325c5fcd8ed8b0e7
2020-DEV-1405 <dev2020.014@gmail.com>
2020-02-29 00:10:20 +0530

    Committing test file having cross marking tests on board covering cross marking, cross marking on blank state, cross marking over exiting cross state marked over board

--- src/test/java/com/tictactoe/kata/TicTacToeKataApplicationTests.java
+++ src/test/java/com/tictactoe/kata/TicTacToeKataApplicationTests.java
@@ -1,10 +1,44 @@
 package com.tictactoe.kata;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tictactoe.kata.game.TicTacToeGameProcessor;
+
 /**
  * 
  * @author AKS1405
  *
  */
 public class TicTacToeKataApplicationTests {
-	
+	private TicTacToeGameProcessor ticTacToe;
+
+	@Before
+	public void setUp() {
+		ticTacToe = new TicTacToeGameProcessor();
+	}
+
+	@Test
+	public void testCrossMarking() {
+		ticTacToe.markCross(0, 1);
+
+		assertEquals(true, ticTacToe.isCross(0, 1));
+	}
+
+	@Test
+	public void testMarkCrossOnBlank() {
+		boolean crossPlaced = ticTacToe.markCross(1, 2);
+
+		assertEquals(true, crossPlaced);
+	}
+
+	@Test
+	public void testMarkingCrossOverExistingMarkedCross() {
+		ticTacToe.markCross(2, 1);
+		boolean crossPlaced = ticTacToe.markCross(2, 1);
+
+		assertEquals(false, crossPlaced);
+	}	
 }
