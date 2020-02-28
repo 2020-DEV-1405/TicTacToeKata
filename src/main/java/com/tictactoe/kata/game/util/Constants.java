ff917f5a371075b3baf4d9a20c6d252346da68ac
2020-DEV-1405 <dev2020.014@gmail.com>
2020-02-29 01:55:48 +0530

    Adding constants class for refectoring of code and removing repeatative strings

---
+++ src/main/java/com/tictactoe/kata/game/util/Constants.java
@@ -0,0 +1,14 @@
+package com.tictactoe.kata.game.util;
+
+
+/**
+ * 
+ * @author AKS1405
+ *
+ */
+public class Constants {
+	public static final String ENTER_ROW = " : Please enter Row position";
+	public static final String ENTER_COLUMN = " : Please enter Column position";
+	public static final String POSITION_OCCUPIED =  "\n Row & Column position is already occupied, Please re-enter.. \n";
+	public static final String INCORRECT_ROW_COL_VALUE =  "\n Incorrect Row & Column values provided, Please re-enter \n";
+}
